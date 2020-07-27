89758ecd9fecb9c316ee49ffc6204b8e2c37dcd6
prashant sharma <prashant.sharma@enquero.com>
2020-07-28 01:11:33 +0530

    added custom event and handler to get the step clicked

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 const steps = [
   {
-    label: 'One',
-    icon: '1'
+    label: "One",
+    icon: "1",
   },
   {
-    label: 'Two',
-    icon: '2'
+    label: "Two",
+    icon: "2",
   },
   {
-    label: 'Three',
-    icon: '3'
-  }
-]
+    label: "Three",
+    icon: "3",
+  },
+];
+
+const currentStep = {
+  id: 1,
+  label: "One",
+  icon: "1",
+};
+
+onStepClick = (step) => {
+  console.log(step)
+}
 
 var stringified = JSON.stringify(steps);
-var el = document.getElementsByTagName('wc-stepper')[0]
-el.setAttribute('steps', stringified)
+var currentStepStr = JSON.stringify(currentStep);
+
+var el = document.getElementsByTagName("wc-stepper")[0];
+
+el.setAttribute("steps", stringified);
+el.setAttribute("currentStep", currentStepStr);
+
+el.addEventListener('onStepClick', (e) => {
+  console.log(e.detail.step());
+})
--- stepper.js
+++ stepper.js
@@ -2,6 +2,13 @@ class Stepper extends HTMLElement {
   constructor() {
     super();
     this._steps = [];
+    this._currentStep = null;
+    this.clickedStep = null;
+    this.onStepClick = new CustomEvent("onStepClick", {
+      bubbles: true,
+      composed: true,
+      detail: { step: () => this.clickedStep || "wololo" },
+    });
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
         <style>
@@ -11,9 +18,11 @@ class Stepper extends HTMLElement {
             min-width: 30rem;
             justify-content: center;
             width: 30rem;
-            background-color: #a7a7a7;
+            background-color: #ffffff;
             padding: 1rem;
             border-radius: 4px;
+            border: 1px solid;
+            box-shadow: 2px 2px 8px 0px;
           }
           .step{
             display: flex;
@@ -28,6 +37,7 @@ class Stepper extends HTMLElement {
             text-align: center;
             height: 2rem;
             font-weight: 600;
+            cursor: pointer;
           }
           .step-label{
             letter-spacing: 1px;
@@ -41,6 +51,9 @@ class Stepper extends HTMLElement {
             min-width: 20%;
             align-self: center;
             margin-bottom: 1rem;
+          }
+          .step__iscomplete{
+            background-color: #06BEE1;
           }
         </style>
         <div id="stepper_container">
@@ -56,7 +69,7 @@ class Stepper extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ["steps"];
+    return ["steps", "currentStep"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -66,26 +79,43 @@ class Stepper extends HTMLElement {
 
     if (name === "steps") {
       this._steps = JSON.parse(newValue);
-      this._renderSteps();
     }
+
+    if (name === "currentStep") {
+      this._currentStep = JSON.parse(newValue);
+    }
+
+    this._renderSteps();
   }
 
-  _attachStep(step,index,  fragment) {
+  handleStepClick = (event) => {
+    let parts = event.target.id.split("_");
+    this.clickedStep = this._steps[parts[parts.length - 1]];
+    this.dispatchEvent(this.onStepClick);
+  };
+
+  _attachStep(step, index, fragment) {
     let stepTemplate = `
       <div class='step'>
-        <div class='step-icon'>${step.icon}</div>
+        <div id="__step_${index}" class='step-icon'>${step.icon}</div>
         <div class='step-label'>${step.label}</div>
       <div>
     `;
 
-    let connector = '<div class="connector"></div>'
+    let connector = '<div class="connector"></div>';
 
-    if(index === 0){
-      return fragment.insertAdjacentHTML('beforeend', stepTemplate)
+    if (index !== 0) {
+      fragment.insertAdjacentHTML("beforeend", connector);
+    }
+    fragment.insertAdjacentHTML("beforeend", stepTemplate);
+
+    if (index === 0) {
+      let el = fragment.querySelector(".step-icon");
+      el.classList.add("step__iscomplete");
     }
 
-    fragment.insertAdjacentHTML('beforeend', connector)
-    fragment.insertAdjacentHTML('beforeend', stepTemplate)
+    let el1 = fragment.querySelector(`#__step_${index}`);
+    el1.addEventListener("click", this.handleStepClick);
   }
 
   _renderSteps() {
